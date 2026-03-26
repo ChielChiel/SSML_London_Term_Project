49b8f76e6da7eff803ad2606851e237be9abc0e9
Daan <79722563+DaanK0123@users.noreply.github.com>
2026-03-26 12:55:11 +0100

    NDVI data is in the data file

--- adding_predictors.R
+++ adding_predictors.R
@@ -7,9 +7,11 @@ setwd(this.path::here())
 data <- st_read("data/ward_poly.gpkg")
 brownfield.data <- read.csv("data/brownfield_and_noise/areas/brownfield.csv")
 noise.data <- read.csv("data/brownfield_and_noise/areas/noise_polution.csv")
-
+ndvi.data <- read.csv("data/outputs_ndvi_2013/ward_ndvi_annual_2013.csv")|>
+  select(-months_with_data)
 
 data <- left_join(data, brownfield.data, by = join_by(New.ward.code == GSS_CODE), suffix = c("", ""))
 data <- left_join(data, noise.data, by = join_by(New.ward.code == GSS_CODE), suffix = c("", ""))
+data <- left_join(data, ndvi.data, join_by(New.ward.code), suffix = c("", ""))
 
 st_write(data, dsn = 'data/ward_poly.gpkg', delete_dsn = TRUE)
